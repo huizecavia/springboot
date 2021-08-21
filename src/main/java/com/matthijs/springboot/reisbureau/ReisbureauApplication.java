895824bd86b31dfb616c14b93f22f6e52606cdb1
huizecavia <bomaltennishal@outlook.com>
2021-08-21 13:37:05 +0200

    eerste versie - nieuw

---
+++ src/main/java/com/matthijs/springboot/reisbureau/ReisbureauApplication.java
@@ -0,0 +1,13 @@
+package com.matthijs.springboot.reisbureau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReisbureauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReisbureauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matthijs/springboot/reisbureau/ReisbureauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthijs.springboot.reisbureau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReisbureauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
